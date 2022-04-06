8887995dea0f785d01c9576337fb8b66e4ad114b
tatuhikokoyama <tatuhiko456321@gmail.com>
2022-04-06 12:25:05 +0900

    Create FREE-CAR-Texture.png.meta

---
+++ Assets/ItemDelete.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDelete : MonoBehaviour
+{
+    //Unityちゃんのオブジェクト
+    private GameObject unitychan;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Unityちゃんのオブジェクトを取得
+        this.unitychan = GameObject.Find("unitychan");
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //オブジェクトがUniyuchanの後方へ出たら破棄する
+        if (unitychan.transform.position.z - transform.position.z > 2.5f)
+        {
+            //アイテムを破棄
+            Destroy(this.gameObject);
+        }
+
+        Debug.Log("相対距離: " + (unitychan.transform.position.z - transform.position.z));
+    }
+}
